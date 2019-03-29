7b1673b5e67dcf178ffa2e8a0b3713286abff454
Travis CI User <travis@example.org>
2019-03-29 18:50:17 +0000

    Documentation upload

---
+++ actix/io/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["FramedWrite","A wrapper for the `AsyncWrite` and `Encoder` types. The AsyncWrite will be flushed when this struct is dropped."],["Writer","A wrapper for `AsyncWrite` types."]],"trait":[["WriteHandler","A helper trait for write handling."]]});
